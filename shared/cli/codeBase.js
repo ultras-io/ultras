9c4cf4738b602ae9f704814bd61dac167a72d757
Hayk <hay92k@gmail.com>
2021-09-15 23:07:23 +0400

    add cli script for creating component

---
+++ shared/cli/codeBase.js
@@ -0,0 +1,57 @@
+'use strict';
+
+
+const getTypesBaseCode = (name) => `
+export type ${name}Props = {
+    title: string;
+};
+`;
+
+const getStylesBaseCode = () => `
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+container: {
+    padding: 8,
+    alignSelf: 'flex-start',
+},
+text: {
+    fontWeight: '600',
+    textAlign: 'center',
+},
+});
+`;
+
+const getComponentBaseCode = (name) => `
+import React from 'react';
+import {View, Text} from 'react-native';
+
+import {${name}Props} from './types';
+
+import styles from './styles';
+
+const ${name}: React.FC<${name}Props> = ({title}) => {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>{title}</Text>
+        </View>
+    );
+};
+
+export default ${name};
+`;
+
+const getExportsBaseCode = (name) => `
+import ${name} from './${name}';
+export default ${name};
+
+export * from './types';
+export * from './${name}';
+`;
+
+module.exports = {
+    getTypesBaseCode,
+    getStylesBaseCode,
+    getComponentBaseCode,
+    getExportsBaseCode
+};
