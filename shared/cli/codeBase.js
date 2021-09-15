7c0208315ccc2767e2607df09755701e3308672a
Hayk <hay92k@gmail.com>
2021-09-16 00:43:39 +0400

    change type to interface

--- shared/cli/codeBase.js
+++ shared/cli/codeBase.js
@@ -2,7 +2,7 @@
 
 
 const getTypesBaseCode = (name) => `
-export type ${name}Props = {
+export interface I${name}Props {
     title: string;
 };
 `;
@@ -11,14 +11,14 @@ const getStylesBaseCode = () => `
 import {StyleSheet} from 'react-native';
 
 export default StyleSheet.create({
-container: {
-    padding: 8,
-    alignSelf: 'flex-start',
-},
-text: {
-    fontWeight: '600',
-    textAlign: 'center',
-},
+    container: {
+        padding: 8,
+        alignSelf: 'flex-start',
+    },
+    text: {
+        fontWeight: '600',
+        textAlign: 'center',
+    },
 });
 `;
 
@@ -26,11 +26,11 @@ const getComponentBaseCode = (name) => `
 import React from 'react';
 import {View, Text} from 'react-native';
 
-import {${name}Props} from './types';
+import {I${name}Props} from './types';
 
 import styles from './styles';
 
-const ${name}: React.FC<${name}Props> = ({title}) => {
+const ${name}: React.FC<I${name}Props> = ({title}) => {
     return (
         <View style={styles.container}>
             <Text style={styles.text}>{title}</Text>
