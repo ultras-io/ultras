78867f70e6988956c9b88c7744de299eb188b36c
Hayk <hay92k@gmail.com>
2021-10-11 00:13:08 +0400

    Add Comments Count Component

---
+++ clients/app/src/views/components/base/CommentsCount/styles.ts
@@ -0,0 +1,20 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    width: 24,
+    height: 24,
+  },
+  commentsView: {
+    position: 'absolute',
+    justifyContent: 'center',
+    width: '100%',
+    height: 20,
+    paddingHorizontal: 2,
+  },
+  comments: {
+    fontWeight: 'bold',
+    fontSize: 10,
+    textAlign: 'center',
+  },
+});
