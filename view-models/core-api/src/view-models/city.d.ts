e879e8902217d2b58117b4ee58debe873d85a824
rado196 <rado196b@gmail.com>
2022-03-18 01:01:03 +0400

    test file removed

--- view-models/core-api/src/view-models/city.d.ts
+++
@@ -1,6 +0,0 @@
-import { CountryViewModel } from './country';
-export declare type CityViewModel = ViewModel<{
-    name: string;
-    country: CountryViewModel;
-}>;
-export declare type CitiesViewModel = Array<CityViewModel>;
