f11b5568e3f429bc9413a37dcb409b00d0c45d00
hay92k <haykblrtsyan@gmail.com>
2022-05-22 19:25:28 +0400

    [refactor] Change common screens options (#110)
    Co-authored-by: Hayk <hay92k@gmail.com>

--- clients/app/src/views/navigation/screens/commonScreens.tsx
+++ clients/app/src/views/navigation/screens/commonScreens.tsx
@@ -1,7 +1,4 @@
 import React from 'react';
-
-import { ComponentType } from 'react';
-
 import Match from 'views/screens/Match';
 import Event from 'views/screens/Event';
 import Post from 'views/screens/Post';
@@ -11,110 +8,89 @@ import FanClubAbout from 'views/screens/FanClubAbout';
 import Profile from 'views/screens/Profile';
 import NewEvent from 'views/screens/NewEvent';
 import ProfileList from 'views/screens/ProfileList';
-
 import UltrasText from 'views/components/base/UltrasText';
-
 import type { CommonScreens } from '../types';
 import styles from 'styles/styles';
-
-// don't touch ordering
-const SCREENS: CommonScreens = {
-  match: {
-    name: 'Match',
-    component: Match,
-  },
-  event: {
-    name: 'Event',
-    component: Event,
-  },
-  post: {
-    name: 'Post',
-    component: Post,
-  },
-  team: {
-    name: 'Team',
-    component: Team,
-  },
-  fanClub: {
-    name: 'FanClub',
-    component: FanClub,
-  },
-  profile: {
-    name: 'Profile',
-    component: Profile,
-    headerTitle: () => (
-      <UltrasText style={styles.headerLogo} color="primary">
-        ultras
-      </UltrasText>
-    ),
-  },
-  profileList: {
-    name: 'ProfileList',
-    component: ProfileList,
-  },
-  newEvent: {
-    name: 'NewEvent', // @TODO change to create Event
-    component: NewEvent,
-    isModal: true,
-    headerShown: false,
-  },
-  fanClubAbout: {
-    name: 'FanClubAbout',
-    component: FanClubAbout,
-    isModal: true,
-    headerShown: true,
-  },
-};
 
 const defaultOptions = {
   headerTitle: '',
   headerBackTitle: '',
 };
 
-const generateScreen = (
-  tabName: string,
-  screenName: string,
-  Stack: any,
-  component: ComponentType<any>,
-  options: any = defaultOptions
-) => (
-  <Stack.Screen
-    key={`${tabName}:${screenName}`}
-    name={`${tabName}:${screenName}`}
-    component={component}
-    initialParams={{ tabName }}
-    options={options}
-  />
-);
-
-export const generateCommonScreens = (tabName: string, Stack: any) => (
-  <React.Fragment>
-    {Object.values(SCREENS).map(item => {
-      if (!item.isModal) {
-        return generateScreen(
-          tabName,
-          item.name,
-          Stack,
-          item.component,
-          item.headerTitle
-            ? {
-                headerTitle: item.headerTitle,
-              }
-            : defaultOptions
-        );
-      }
-    })}
-
-    <Stack.Group screenOptions={{ presentation: 'modal' }}>
-      {Object.values(SCREENS).map(item => {
-        if (item.isModal) {
-          return generateScreen(tabName, item.name, Stack, item.component, {
-            headerShown: item.headerShown,
-          });
-        }
-      })}
-    </Stack.Group>
-  </React.Fragment>
-);
+// don't touch ordering
+const SCREENS: CommonScreens = {
+  match: {
+    name: 'Match',
+    component: Match,
+    options: {
+      ...defaultOptions,
+    },
+  },
+  event: {
+    name: 'Event',
+    component: Event,
+    options: {
+      ...defaultOptions,
+    },
+  },
+  post: {
+    name: 'Post',
+    component: Post,
+    options: {
+      ...defaultOptions,
+    },
+  },
+  team: {
+    name: 'Team',
+    component: Team,
+    options: {
+      ...defaultOptions,
+    },
+  },
+  fanClub: {
+    name: 'FanClub',
+    component: FanClub,
+    options: {
+      ...defaultOptions,
+    },
+  },
+  profile: {
+    name: 'Profile',
+    component: Profile,
+    options: {
+      ...defaultOptions,
+      headerTitle: () => (
+        <UltrasText style={styles.headerLogo} color="primary">
+          ultras
+        </UltrasText>
+      ),
+    },
+  },
+  profileList: {
+    name: 'ProfileList',
+    component: ProfileList,
+    options: {
+      ...defaultOptions,
+    },
+  },
+  newEvent: {
+    name: 'NewEvent', // @TODO change to create Event
+    component: NewEvent,
+    options: {
+      ...defaultOptions,
+      presentation: 'modal',
+      headerShown: false,
+    },
+  },
+  fanClubAbout: {
+    name: 'FanClubAbout',
+    component: FanClubAbout,
+    options: {
+      ...defaultOptions,
+      presentation: 'modal',
+      headerShown: false,
+    },
+  },
+};
 
 export default SCREENS;
