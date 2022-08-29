305149c13a9aab8f8ae64dfd0fa3e7a85b8e644b
rado196 <rado196b@gmail.com>
2022-08-29 22:39:42 +0400

    firebase push notification with react native

---
+++ clients/app/ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by Rado on 8/27/22.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
