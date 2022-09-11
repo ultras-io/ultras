6b5a2e297f07c6f679a63b6fc4bffa36dcf0f5a6
rado196 <rado196b@gmail.com>
2022-09-11 13:30:45 +0400

    .npmrc and .yarnrc files added, bundle id updated

--- clients/app/android/app/src/main/java/io/ultras/MainActivity.java
+++ clients/app/android/app/src/main/java/io/ultras/MainActivity.java
@@ -1,4 +1,4 @@
-package com.app;
+package io.ultras;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "app";
+    return "ultras";
   }
 }
