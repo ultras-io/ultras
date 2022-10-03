719c343791258d88419f49c83701c34502875889
rado196 <rado196b@gmail.com>
2022-10-03 22:42:53 +0400

    back button updated

--- clients/app/src/views/screens/CreateEvent/screens/SelectMatch/SelectMatch.tsx
+++ clients/app/src/views/screens/CreateEvent/screens/SelectMatch/SelectMatch.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button } from 'native-base';
-import { useTheme } from 'themes';
-import I18n from 'i18n/i18n';
+import { Icons } from 'assets/icons';
+import Icon from 'views/components/base/Icon';
 import useNavigationWithParams from 'utils/hooks/useNavigationWithParams';
 import { ISelectMatchProps } from './types';
 
@@ -12,21 +12,25 @@ const SelectMatchContainer = React.lazy(
 const SelectMatch: React.FC<ISelectMatchProps> = ({ route }) => {
   const { matchId } = route.params;
 
-  const { colors } = useTheme();
   const { goBack } = useNavigationWithParams();
 
   return (
     <>
       <Button
-        onPress={goBack}
         variant={'empty'}
-        alignSelf="flex-start"
-        _text={{ color: colors.textAction }}
-        mt={'5'}
-        mb={'2.5'}
-        px={'2.5'}
+        alignSelf={'flex-start'}
+        marginTop={'5'}
+        marginBottom={'2.5'}
+        paddingX={'2.5'}
+        onPress={goBack}
       >
-        {I18n.t('common-close')}
+        <Icon
+          marginBottom={'2.5'}
+          marginX={'2.5'}
+          name={Icons.Back}
+          color="textAction"
+          size="sm"
+        />
       </Button>
 
       <SelectMatchContainer matchId={matchId} />
