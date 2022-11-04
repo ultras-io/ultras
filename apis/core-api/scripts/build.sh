95e296c8140ac287565dff86e98b70d21a233351
rado196 <rado196b@gmail.com>
2022-11-04 23:26:22 +0400

    [fix] build and run pm2 script

---
+++ apis/core-api/scripts/build.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+cd "$(dirname "$0")/../" || exit 1
+
+function check_exit() {
+  local EXIT_CODE=$1
+  local MESSAGE=$2
+
+  if [[ $? != 0 ]]; then
+    echo ">>"
+    echo ">> $MESSAGE"
+    echo ">>"
+
+    exit $EXIT_CODE
+  fi
+}
+
+# install node modules if missing
+if [[ ! -d node_modules ]]; then
+  npm install
+  check_exit $? 'Failed to install node modules.'
+fi
+
+# build project
+export NODE_OPTIONS="--max-old-space-size=4096"
+./node_modules/.bin/tsc --rootDir src
+check_exit $? 'Failed to build project.'
+
+# replace folders
+if [[ -d dist ]]; then
+  rm -rf dist
+fi
+
+mv build dist
+
+# restart pm2
+pm2 restart ecosystem.config.js
+pm2 save --force
