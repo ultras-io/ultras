21df5824363c3c2cccacab4992a796a8d0d12f40
rado196 <rado196b@gmail.com>
2022-12-13 21:45:37 +0400

    [fix] Added auto-close functionality for date-picker

--- clients/app/src/views/components/base/DateTimePicker/DateTimePicker.tsx
+++ clients/app/src/views/components/base/DateTimePicker/DateTimePicker.tsx
@@ -1,51 +1,31 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
-import DateTimePicker from '@react-native-community/datetimepicker';
-import { Box, Text } from 'native-base';
+import PickerWrapper from './PickerWrapper';
 import { IDatePickerProps } from './types';
 
-export const DatePicker: React.FC<IDatePickerProps> = ({ text, value, onChange }) => {
+export const DatePicker: React.FC<IDatePickerProps> = ({
+  autoclose = true,
+  ...restProps
+}) => {
   return (
-    <Box style={styles.relative}>
-      <Text variant={'matchDate'}>{text}</Text>
-      <Box style={styles.hidden}>
-        <DateTimePicker
-          value={value}
-          mode={'date'}
-          onChange={onChange}
-          minimumDate={new Date()}
-          display="compact"
-          themeVariant="dark"
-        />
-      </Box>
-    </Box>
+    <PickerWrapper
+      mode={'date'}
+      minimumDate={new Date()}
+      autoclose={autoclose}
+      {...restProps}
+    />
   );
 };
 
-export const TimePicker: React.FC<IDatePickerProps> = ({ text, value, onChange }) => {
+export const TimePicker: React.FC<IDatePickerProps> = ({
+  autoclose = false,
+  ...restProps
+}) => {
   return (
-    <Box style={styles.relative}>
-      <Text variant={'matchDate'}>{text}</Text>
-      <Box style={styles.hidden}>
-        <DateTimePicker
-          value={value}
-          mode={'time'}
-          onChange={onChange}
-          minuteInterval={10}
-          display="compact"
-          themeVariant="dark"
-        />
-      </Box>
-    </Box>
+    <PickerWrapper
+      mode={'time'}
+      minuteInterval={10}
+      autoclose={autoclose}
+      {...restProps}
+    />
   );
 };
-
-const styles = StyleSheet.create({
-  relative: { position: 'relative' },
-  hidden: {
-    position: 'absolute',
-    width: '100%',
-    height: '100%',
-    opacity: 0,
-  },
-});
